4d3150c2438ad82847c03144dedfbd47d99e4ec1
RISHU <studentcod072@gmail.com>
2026-07-30 20:48:23 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.23%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,9 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
-        int gm = 0;
-        for(int i = 0; i<word.length();i++){
-            gm +=i/8+1;
+        int g = 0;
+        for(int i = 0; i<word.length(); i++){
+
+            g += i/8+1;
         }
-        return gm;
+        return g;
     }
 }
