d63318c49e93fe95449f8850877bd27fd890b8a6
RISHU <studentcod072@gmail.com>
2026-09-25 08:16:47 +0530

    Time: 11 ms (47.2%), Space: 47.2 MB (64%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -1,30 +1,54 @@
 class Solution {
+    private int i;
+    private String s;
+
     public List<String> braceExpansionII(String expression) {
-        Set<String> result = new HashSet<>();
-        Deque<String> queue = new ArrayDeque<>();
-        queue.offer(expression);
-
-        while (!queue.isEmpty()) {
-            String cur = queue.poll();
-            int close = cur.indexOf('}');
-            if (close == -1) {          // no braces left → a final expansion
-                result.add(cur);
-                continue;
-            }
-            int open = close;
-            while (cur.charAt(open) != '{') open--;
-
-            String prefix = cur.substring(0, open);
-            String suffix = cur.substring(close + 1);
-            String[] options = cur.substring(open + 1, close).split(",");
-
-            for (String opt : options) {
-                queue.offer(prefix + opt + suffix);
-            }
-        }
-
-        List<String> ans = new ArrayList<>(result);
+        s = expression;
+        i = 0;
+        Set<String> res = parseExpr();
+        List<String> ans = new ArrayList<>(res);
         Collections.sort(ans);
         return ans;
+    }
+
+    // expr := term (',' term)*
+    private Set<String> parseExpr() {
+        Set<String> result = new HashSet<>(parseTerm());
+        while (i < s.length() && s.charAt(i) == ',') {
+            i++; // skip ','
+            result.addAll(parseTerm());
+        }
+        return result;
+    }
+
+    // term := factor+   (concatenation → Cartesian product)
+    private Set<String> parseTerm() {
+        Set<String> result = new HashSet<>();
+        result.add(""); // identity for product
+
+        while (i < s.length() && s.charAt(i) != ',' && s.charAt(i) != '}') {
+            Set<String> factor = parseFactor();
+            Set<String> next = new HashSet<>();
+            for (String a : result)
+                for (String b : factor)
+                    next.add(a + b);
+            result = next;
+        }
+        return result;
+    }
+
+    // factor := '{' expr '}' | letter
+    private Set<String> parseFactor() {
+        if (s.charAt(i) == '{') {
+            i++;                       // skip '{'
+            Set<String> res = parseExpr();
+            i++;                       // skip '}'
+            return res;
+        } else {
+            Set<String> res = new HashSet<>();
+            res.add(String.valueOf(s.charAt(i)));
+            i++;
+            return res;
+        }
     }
 }
