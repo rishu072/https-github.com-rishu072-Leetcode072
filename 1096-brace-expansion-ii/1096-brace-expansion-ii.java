b3d0ff1403676eb01209e0dc029aaf3e93c67194
RISHU <studentcod072@gmail.com>
2026-09-25 08:18:15 +0530

    Time: 9 ms (82.4%), Space: 46.8 MB (89.6%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -1,54 +1,70 @@
 class Solution {
-    private int i;
-    private String s;
-
     public List<String> braceExpansionII(String expression) {
-        s = expression;
-        i = 0;
-        Set<String> res = parseExpr();
-        List<String> ans = new ArrayList<>(res);
+        char[] s = expression.toCharArray();
+        int n = s.length;
+
+        char[] op = new char[n + 1];
+        int opTop = -1;
+
+        ArrayList<Set<String>> stk = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+            if (c == ',') {
+                while (opTop >= 0 && op[opTop] == '*') {
+                    opTop = ope(op, opTop, stk);
+                }
+                op[++opTop] = '+';
+            } else if (c == '{') {
+                if (i > 0) {
+                    char p = s[i - 1];
+                    if (p == '}' || (p >= 'a' && p <= 'z')) op[++opTop] = '*';
+                }
+                op[++opTop] = '{';
+            } else if (c == '}') {
+                while (opTop >= 0 && op[opTop] != '{') {
+                    opTop = ope(op, opTop, stk);
+                }
+                opTop--; // pop '{'
+            } else {
+                if (i > 0) {
+                    char p = s[i - 1];
+                    if (p == '}' || (p >= 'a' && p <= 'z')) op[++opTop] = '*';
+                }
+                Set<String> single = new HashSet<>(2);
+                single.add(String.valueOf(c));
+                stk.add(single);
+            }
+        }
+
+        while (opTop >= 0) {
+            opTop = ope(op, opTop, stk);
+        }
+
+        List<String> ans = new ArrayList<>(stk.get(stk.size() - 1));
         Collections.sort(ans);
         return ans;
     }
 
-    // expr := term (',' term)*
-    private Set<String> parseExpr() {
-        Set<String> result = new HashSet<>(parseTerm());
-        while (i < s.length() && s.charAt(i) == ',') {
-            i++; // skip ','
-            result.addAll(parseTerm());
-        }
-        return result;
-    }
+    // returns new opTop
+    private int ope(char[] op, int opTop, List<Set<String>> stk) {
+        int r = stk.size() - 1;
+        int l = r - 1;
+        Set<String> L = stk.get(l);
+        Set<String> R = stk.get(r);
 
-    // term := factor+   (concatenation → Cartesian product)
-    private Set<String> parseTerm() {
-        Set<String> result = new HashSet<>();
-        result.add(""); // identity for product
-
-        while (i < s.length() && s.charAt(i) != ',' && s.charAt(i) != '}') {
-            Set<String> factor = parseFactor();
-            Set<String> next = new HashSet<>();
-            for (String a : result)
-                for (String b : factor)
-                    next.add(a + b);
-            result = next;
-        }
-        return result;
-    }
-
-    // factor := '{' expr '}' | letter
-    private Set<String> parseFactor() {
-        if (s.charAt(i) == '{') {
-            i++;                       // skip '{'
-            Set<String> res = parseExpr();
-            i++;                       // skip '}'
-            return res;
+        if (op[opTop] == '+') {
+            L.addAll(R);
         } else {
-            Set<String> res = new HashSet<>();
-            res.add(String.valueOf(s.charAt(i)));
-            i++;
-            return res;
+            Set<String> tmp = new HashSet<>(L.size() * R.size() * 2);
+            for (String a : L) {
+                for (String b : R) {
+                    tmp.add(a + b);
+                }
+            }
+            stk.set(l, tmp);
         }
+        stk.remove(r);
+        return opTop - 1;
     }
 }
